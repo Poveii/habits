7df37a7f33bf774a1446d5c6298497bee0e73086
Poveii <yellowtutors@gmail.com>
2023-01-19 17:22:05 -0300

    :wheelchair: Added alternative to Logo img

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img src={Logo} alt="" />
+      <img
+        src={Logo}
+        alt="Logotipo do aplicativo Habits, seis quadrados pequenos em cima do nome habits, tudo em minúsculo, as cores dos quadrados vão do preto ao lilás, passando pelo roxo."
+      />
     </div>
   );
 }
